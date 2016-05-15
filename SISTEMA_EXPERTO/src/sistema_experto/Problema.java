e5e6ec31da607fbd9d87fac8799b9573e9f46b9d
cjgalvisc <cristofergt96@hotmail.es>
2016-05-15 16:10:15 -0500

    base del proyecto

---
+++ SISTEMA_EXPERTO/src/sistema_experto/Problema.java
@@ -0,0 +1,12 @@
+
+package sistema_experto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problema {
+    public String nombre;
+    public Map<String, String> variables= new HashMap<String, String>();
+    public Map<String, String> objetivos= new HashMap<String, String>();
+    
+}
